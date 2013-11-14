4ae9e7b7003788dcc32e8579880d6a063d03c9b3
unknown <rjlegra@stv438-20.ad.ilstu.edu>
2013-11-14 10:44:02 -0600

    Initial Commit

---
+++ XCScoreTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/*
+ * Template for unit tests.
+ * TODO
+ */
+@RunWith(JUnit4.class)
+public class XCScoreTest {
+
+    @Test
+    public void thisAlwaysPasses() {
+
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
