d2c1cf46d8f5517aeeeeef30abd1d0816197a9bd
B0bby <unthinkingdepths+git@gmail.com>
2013-11-16 12:52:06 -0600

    changed development environment

---
+++ src/XCScore.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: b0b
+ * Date: 11/16/13
+ * Time: 12:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class XCScore {
+    public static void main(String args[]){
+        Race race = new Race();
+
+
+    }
+}
---
+++ tests/src/XCScoreTests.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: b0b
+ * Date: 11/16/13
+ * Time: 12:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class XCScoreTests {
+    @org.junit.Test
+    public void testShouldPass(){
+        assert (1 == 1);
+    }
+    @org.junit.Test
+    public void testShouldFail(){
+        assert (1 == 0);
+    }
+    @Test
+    public void playerIsAddedToDatabase(){
+
+    }
+    @Test
+    public void teamIsAddedToDatabase(){
+
+    }
+    @Test
+    public void playerTimeIsUpdated(){
+
+    }
+}
