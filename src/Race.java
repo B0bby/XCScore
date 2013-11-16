afcd8c5f12f85bce7e9f979bf6f3b85e879c3c3d
B0bby <unthinkingdepths+git@gmail.com>
2013-11-16 13:02:45 -0600

    Changed development environment

---
+++ src/Race.java
@@ -0,0 +1,36 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: b0b
+ * Date: 11/16/13
+ * Time: 2:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.ArrayList;
+
+public class Race {
+    private static ArrayList<Team> teams = new ArrayList<Team>();
+    private static ArrayList<Runner> runners = new ArrayList<Runner>();
+
+    public void addRunnerToDatabase(Runner runner){
+
+    }
+    public void removeRunnerFromDatabase(Runner runner){
+
+    }
+    public void updateRunnerTime(Runner runner){
+
+    }
+    public void disqualifyRunner(Runner runner){
+
+    }
+    public void scoreTeams(ArrayList teams){
+
+    }
+    public void rankTeams(ArrayList teams){
+
+    }
+    public void createRace(){
+
+    }
+
+}
---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: b0b
+ * Date: 11/16/13
+ * Time: 10:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Runner {
+}
